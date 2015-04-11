651db08b80f89224d9bce2fa76244e823933e83e
pashaSHER <ilovesherel@mail.ru>
2015-04-11 21:08:25 +0400

    Start

---
+++ Task-1/Task-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinearFunction f = new LinearFunction(-3, 4);
+            Console.WriteLine("Enter the number x");
+            double res;
+            var b = Double.TryParse(Console.ReadLine(), out res);
+
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Value of the function f={0}*x+{1}={2}", f.A,f.B,f.GetY(x));
+            Console.ReadLine();
+        }
+    }
+}
